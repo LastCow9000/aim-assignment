0796779b3efc9236cb355e94372572942cd3640f
LastCow9000 <sys19912002@hanmail.net>
2024-12-19 03:18:00 +0900

    feat: get mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3309,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: process.env.NODE_ENV !== 'production',
+      charset: 'utf8mb4',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
