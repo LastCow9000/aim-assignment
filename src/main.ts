7754c7cf143524c35ec3297bb26adefb6aad6d55
LastCow9000 <sys19912002@hanmail.net>
2024-12-19 03:13:14 +0900

    feat: apply intercepter, filter. pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { LogInterceptor } from './common/interceptors/log.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LogInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(port);
+  console.log(`listening on port ${port}`);
 }
+
 bootstrap();
